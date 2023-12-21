edc78c66b9984725aa81c0dcd500738b4c3832fc
Isaiah Gage <frb32@txstate.edu>
2023-12-21 01:37:50 -0600

    Added Emailsection

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -6,28 +6,31 @@ const ProjectsData = [
         id: 1,
         title: "JustPositivity",
         description: "Project description",
-        image: "/images/projects/1.png"
+        image: "/images/JustPositivity.png",
+        gitUrl: 'https://github.com/Mako11z/JustPositivity',
     },
     {
         id: 2,
         title: "StopHumanTrafficking",
         description: "Project description",
-        image: "/images/projects/1.png"
+        image: "/images/StopHumanTrafficking.png",
+        gitUrl: 'https://github.com/Mako11z/StopHumanTrafficking',
     },
     {
         id: 3,
         title: "VibeTrack",
         description: "Project description",
-        image: "/images/projects/1.png"
+        image: "/images/VibeTrack.png",
+        gitUrl: 'https://github.com/micnguyen-rnb90/VibeTrack',
     }
 ];
 
 const ProjectSection = () => {
     return (
         <div>
-            <h2>My Projects</h2>
-            <div>
-                {ProjectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} />)}
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {ProjectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} />)}
             </div>
         </div>
     );
