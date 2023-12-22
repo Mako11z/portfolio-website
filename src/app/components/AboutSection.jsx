2c8ac779205f6c7cab95c3b1e50d4820335df11c
Isaiah Gage <frb32@txstate.edu>
2023-12-21 21:03:29 -0600

    Added styling to about section image

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -52,7 +52,25 @@ const AboutSection = () => {
     return (
         <section className="text-white mb-20">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about-life-code.png" width={450} height={450}  className=""/>
+                <div className="bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{
+                    border: '1px solid linear-gradient(to bottom right, #5000FF, #FF0080)',
+                    boxSizing: 'border-box', // Include border in the overall size
+                    width: '98%', // Adjust the width as needed
+                    height: '98%', // Adjust the height as needed
+                    paddingTop: '5px', // Padding to create space between the image and the border
+                    paddingRight: '3pxpx',
+                    paddingBottom: '5px',
+                    overflow: 'hidden', // Ensure the border doesn't exceed the wrapper
+                }}>
+            <Image 
+                src="/images/about-life-code.png" 
+                width={540} 
+                height={540}  
+                layout='responsive'
+            />
+        </div>
+                </div>
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">This is where my description of me will go</p>
